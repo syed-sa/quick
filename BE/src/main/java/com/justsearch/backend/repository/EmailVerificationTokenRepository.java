f133de38c8af50c485fb72abc2eb086ca8821647
syed <k.syed200367@gmail.com>
2026-01-24 13:14:25 +0530

    commit email repository formatting

--- BE/src/main/java/com/justsearch/backend/repository/EmailVerificationTokenRepository.java
+++ BE/src/main/java/com/justsearch/backend/repository/EmailVerificationTokenRepository.java
@@ -5,12 +5,13 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.justsearch.backend.model.EmailVerificationToken;
+import com.justsearch.backend.model.User;
 
 public interface EmailVerificationTokenRepository
         extends JpaRepository<EmailVerificationToken, Long> {
 
     Optional<EmailVerificationToken> findByToken(String token);
 
-    void deleteByUser(com.justsearch.backend.model.User user);
+    void deleteByUser(User user);
 
 }
