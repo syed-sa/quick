173b25ff8705ecd9a363c21baa460f2d19ccfd3e
syed <k.syed200367@gmail.com>
2026-01-26 17:05:11 +0530

    commit for notification repo update

--- BE/src/main/java/com/justsearch/backend/repository/NotificationRepository.java
+++ BE/src/main/java/com/justsearch/backend/repository/NotificationRepository.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.justsearch.backend.model.Notification;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    // Additional query methods can be defined here if needed
-    // For example, to find notifications by userId:
-   Notification findByUserId(Long userId);
+
 
    List<Notification> findAllByUserId(Long userId);
 }
