a817bd123c1ae30f1b24c33ca5c63413a851d6df
syed <k.syed200367@gmail.com>
2026-01-30 20:10:57 +0530

    commit for global exception handler

---
+++ BE/src/main/java/com/justsearch/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.justsearch.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<?> handleDuplicateRequest(IllegalStateException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleBadRequest(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+}
