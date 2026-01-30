18ce3adea5633b0d98a172ce74e41e8e8027025a
syed <k.syed200367@gmail.com>
2026-01-30 20:11:09 +0530

    commit for unit test updates

--- BE/src/test/java/com/justsearch/backend/BackendApplicationTests.java
+++ BE/src/test/java/com/justsearch/backend/BackendApplicationTests.java
@@ -2,9 +2,12 @@ package com.justsearch.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(properties = {
     "spring.flyway.enabled=false"
 })
+
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
