1bbf3346f4fe6f1bd734ff4c890802eb8cec6fe6
syed <k.syed200367@gmail.com>
2026-02-02 08:32:27 +0530

    commit for redis cache updates

--- BE/src/main/java/com/justsearch/backend/BackendApplication.java
+++ BE/src/main/java/com/justsearch/backend/BackendApplication.java
@@ -3,11 +3,13 @@ package com.justsearch.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.security.access.AccessDeniedException;
 
 import com.justsearch.backend.config.JwtConfig;
 @SpringBootApplication
 @EnableConfigurationProperties(JwtConfig.class)
+@EnableCaching
 public class BackendApplication {
 
 	public static void main(String[] args) {
