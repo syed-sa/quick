08eeb0302329ee1d18e8b47a27b8374fd03c516d
syed <k.syed200367@gmail.com>
2026-02-11 00:00:12 +0530

    redis cinfig upfdate

--- BE/src/main/java/com/justsearch/backend/config/RedisConfig.java
+++ BE/src/main/java/com/justsearch/backend/config/RedisConfig.java
@@ -4,13 +4,16 @@ import java.time.Duration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Bean
@@ -25,10 +28,19 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(5))
-                .disableCachingNullValues();
-    }
+    public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
 
+        RedisCacheConfiguration defaultConfig =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofMinutes(5))
+                        .disableCachingNullValues();
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(defaultConfig)
+                .withCacheConfiguration(
+                        "service-search",
+                        defaultConfig.entryTtl(Duration.ofMinutes(5))
+                )
+                .build();
+    }
 }
